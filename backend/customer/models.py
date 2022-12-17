66e5e3aa06cf4761a23a12ce00a31fc3aef4eb66
Ali Al Rashini <44705253+SharkooMaster@users.noreply.github.com>
2022-12-17 15:19:01 +0100

    Initial branch commit

---
+++ backend/customer/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Customer(models.Model):
+	name = models.TextField(default="", null=False)
+	email = models.TextField(default="", null=False)
+	password = models.TextField(default="", null=False)
+
+	city = models.TextField(default="", null=False)
+	region = models.TextField(default="", null=False)
---
+++ backend/customer/views.py
@@ -0,0 +1,21 @@
+##########################################
+'''
+	Customer:
+		The customer will be the free user
+		who shops from our merchants.
+		The base functionality for a customer is:
+
+		- Create account
+		- Signin
+		- forgot password
+		- Order
+		- Add to favorites
+		- Logout
+		
+		Other functionality that the customer
+		does not have access to but the app needs
+		to handle the customer are sessions.
+'''
+##########################################
+
+from django.http import HttpResponse
