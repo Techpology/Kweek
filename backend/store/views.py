104ff4929cf09818d77e16b6546b07723f50c438
Ali Al Rashini <44705253+SharkooMaster@users.noreply.github.com>
2023-01-05 00:05:29 +0100

    Reverse manage products

--- backend/store/views.py
+++ backend/store/views.py
@@ -221,7 +221,7 @@ def get_products(request):
 		
 		_store = query[0].store
 		_prods = Product.objects.filter(store=_store).all().values()
-		ret = json.dumps(list(_prods))
+		ret = json.dumps(list(_prods).reverse())
 		print(ret)
 		return HttpResponse(ret, status=200)
 	return HttpResponse("Invalid request", status=409)
